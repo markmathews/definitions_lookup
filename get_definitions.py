52e302c55d55139646e9e8339993656bcc937d69
markmathews <mm2121@cam.ac.uk>
2018-02-24 22:15:40 +0000

    minor changes

--- get_definitions.py
+++ get_definitions.py
@@ -1,39 +1,31 @@
 #Python script to get word definitions from the Oxford Dictionary API and store them in a text file
 
-import os
 import requests as r
 import json
-from pprint import pprint
 
-WORDS = ['forebrain','occipital','temporal lobes','gyrus','sulcus']
+WORDS = ['forebrain','occipital','temporal_lobe','gyrus','sulcus','hippocampus','inhibitory_neurons']
 
-base_url = 'https://od-api.oxforddictionaries.com:443/api/v1/'
 app_id = 'b62d6a70'
 app_key = '0b65d2166229a7e856cb85f797d8bcb9'
-function = 'entries'
-language = 'en'
+headers = {'app_id':app_id, 'app_key':app_key}
 
-output = ''
+base_url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/' #last one is language
+
+output = 'DEFINITIONS\n\n'
 
 for i in range(0,len(WORDS)):
-	headers = {'app_id':app_id, 'app_key':app_key}
-	url = base_url + function + '/' + language + '/' + WORDS[i].lower()
-	#print(url)
+	url = base_url + WORDS[i].lower()
+	#print(url) 
 	response = r.get(url, headers=headers)
-	response_json = response.json()
 
-	definition = response_json['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
-	output = output + WORDS[i] + '\n' + definition + '\n\n'
+	if response.status_code == r.codes.ok: #If request goes through (HTTP code 200)
+		response_json = response.json()
+		definition = response_json['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0] #access word definition
+		output += WORDS[i].title().replace("_", " ") + ':\n' + definition.capitalize() + '\n\n' #formatting
 
 
 filename = 'definitions.txt' 
 
-if os.path.exists(filename):
-    append_write = 'a' # append if already exists
-else:
-    append_write = 'w'
-
-f = open(filename,append_write)
+f = open(filename,'w')
 f.write(output)
 f.close()
-	
